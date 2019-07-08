b3547ded5e839d0b9b9bc189e428c0a316cc7744
francis <zfc9d3f@zoho.com>
2019-07-08 08:13:07 -0700

    blah

--- src/components/path/about.js
+++ src/components/path/about.js
@@ -4,23 +4,67 @@ import "./about.css"
 const About = () => (
   <div className="section">
     <div className="about-container">
-      <h1>francis chang</h1>
-      <h3>
-        <span className="red">is</span>
-        <span className="orange">(</span>korean, taiwanese, american
-        <span className="orange">)</span>
-      </h3>
-      <h3>born(new Date("June 20, 1992")).in(california).raise(texas)</h3>
-      <h3>love(art, basketball, swimming)</h3>
-      <h3>
-        {"{"}
-        <br />
-        <span className="indent">favorite movie: pulp fiction;</span>
-        <br />
-        <span className="indent">favorite show:gravity falls</span>
-        <br />
-        {"}"}
-      </h3>
+      <span className="const">const</span> zfc9d3f ={" "}
+      <span className="new">new</span> Developer(<span className="new">"</span>
+      <span className="quotes">francis chang</span>
+      <span className="new">"</span>)
+      <br />
+      <span className="margin" />
+      <span className="new">.</span>
+      <span className="func">is</span>(korean, taiwanese, american)
+      <br />
+      <span className="margin" />
+      <span className="new">.</span>
+      <span className="func">born</span>(<span className="new">new</span> Date(
+      <span className="new">"</span>
+      <span className="quotes">June 20, 1992</span>
+      <span className="new">"</span>)<span className="new">,</span> california)
+      <br />
+      <span className="margin" />
+      <span className="new">.</span>
+      <span className="func">raisedIn</span>(texas)
+      <br />
+      <span className="margin" />
+      <span className="new">.</span>
+      <span className="func">setCurrentAge</span>(
+      <span className="num">27</span>)
+      <br />
+      <span className="margin" />
+      <span className="new">.</span>
+      <span className="func">setFavorites</span>(
+      <span className="new">{"{"}</span>
+      <br />
+      <span className="margin" />
+      <span className="margin" />
+      movie<span className="new">: "</span>
+      <span className="quotes">pulp fiction</span>
+      <span className="new">",</span>
+      <br />
+      <span className="margin" />
+      <span className="margin" />
+      show<span className="new">: "</span>
+      <span className="quotes">gravity falls</span>
+      <span className="new">",</span>
+      <br />
+      <span className="margin" />
+      <span className="margin" />
+      sport<span className="new">: "</span>
+      <span className="quotes">basketball</span>
+      <span className="new">",</span>
+      <br />
+      <span className="margin" />
+      <span className="margin" />
+      music<span className="new">: "</span>
+      <span className="quotes">deep house</span>
+      <span className="new">"</span>
+      <br />
+      <span className="margin" />
+      <span className="new">}</span>)
+      <br />
+      <br />
+      zfc9d3f<span className="new">.</span>
+      <span className="func">findEmployment</span>()
+      <span className="new">;</span>
     </div>
   </div>
 )
--- src/components/path/statement.js
+++ src/components/path/statement.js
@@ -7,23 +7,36 @@ const Statement = () => {
       <div className="statement-container">
         <h3>
           despite academic performance decline in college, i have regained
-          appreciation for software development and computer science in recent
-          years
+          appreciation for{" "}
+          <span className="higlight">software development</span> and{" "}
+          <span className="higlight">computer science</span> in recent years
         </h3>
         <h3>
-          my biggest strength is making dashboard interfaces using React,
-          Typescript and D3, although i also consider myself strong in backend
-          development with Python and CSS
+          my biggest strength is making dashboard interfaces using{" "}
+          <span className="higlight">React</span>,{" "}
+          <span className="higlight">Typescript</span> and{" "}
+          <span className="higlight">D3</span>, although i also consider myself
+          competent in backend development with{" "}
+          <span className="higlight">Python</span> and{" "}
+          <span className="higlight">CSS</span>
         </h3>
-        <h3>my priority right now is strengthening myself as a developer</h3>
+        <h3>
+          my priority right now is{" "}
+          <span className="higlight">strengthening</span> myself as a developer
+        </h3>
         <h3>
           upon employment, i will be learning these technologies; independently,
           if my work does not require it
         </h3>
         <div className="learn-listing">
-          Building performance critical web systems with Rust and GraphQL
+          Building <span className="higlight">performance critical</span> web
+          systems with <span className="higlight">Rust</span> and{" "}
+          <span className="higlight">GraphQL</span>
         </div>
-        <div className="learn-listing">Strengthening CSS and Design skills</div>
+        <div className="learn-listing">
+          Strengthening <span className="higlight">CSS</span> and{" "}
+          <span className="higlight">Design</span> skills
+        </div>
         <h3>
           software development comprises of almost 70 percent of my life right
           now, and i can easily transfer my skills to employment
